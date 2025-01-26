f2e8fd30a6d71a423d9ba27b306e505cd4a114a8
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-26 15:00:42 +0700

    FEAT: if search, filter, and category change fecth product

--- src/layouts/partials/navbar.jsx
+++ src/layouts/partials/navbar.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FilterButton } from '../../components/filter-button'
 import { SearchInput } from '../../components/search-input'
 import { Tabs } from '../../components/tabs'
+import { useProduct } from '../../context/product-context'
 
 export function Navbar() {
   const [activeTab, setActiveTab] = useState(0)
+  const [search, setSearch] = useState('')
+  const [category, setCategory] = useState('')
+  const [filter, setFilter] = useState('')
+  const { fetchProduct } = useProduct()
 
   const handleTabChange = (tab) => {
     setActiveTab(tab)
   }
+
+  useEffect(() => {
+    fetchProduct({ size: 9, search, category, filter })
+  }, [search, category, filter])
 
   return (
     <div className="navbar bg-base-100">
@@ -16,8 +25,8 @@ export function Navbar() {
         <Tabs activeTab={activeTab} setActiveTab={handleTabChange} />
       </div>
       <div className="flex-none gap-2">
-        <SearchInput />
-        <FilterButton />
+        <SearchInput setSearch={setSearch} />
+        <FilterButton setFilter={setFilter} />
       </div>
     </div>
   )
