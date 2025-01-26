f0bd364945645dccd0465e458d0972907d505e90
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-26 14:54:48 +0700

    FEAT: fect product if component mount

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react'
 import { Card } from '../components/card'
 import Container from '../components/container'
+import { useProduct } from '../context/product-context'
 
 export default function Home() {
+  const { products, fetchProduct } = useProduct()
+
+  useEffect(() => {
+    fetchProduct({ size: 9 })
+  }, [])
+  useEffect(() => {
+    console.log(products)
+  }, [products])
+
   return (
     <Container className="min-h-screen">
       <div className="grid grid-cols-3 gap-6">
-        {Array.from({ length: 9 }).map((_, i) => (
-          <Card key={i} />
+        {products.map((product, i) => (
+          <Card key={i} product={product} />
         ))}
       </div>
     </Container>
