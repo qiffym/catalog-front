f704bf9dd4f40bd628d07b695f542b003692c711
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-30 20:30:21 +0700

    FIX: don't use ...

--- src/components/paging.jsx
+++ src/components/paging.jsx
@@ -4,9 +4,7 @@ const Paging = ({ page, totalPage, handleNextPage, handlePrevPage }) => {
       <button className="btn join-item" onClick={handlePrevPage} disabled={page === 1}>
         «
       </button>
-      {page > 1 && <button className="btn join-item">...</button>}
       <button className="btn join-item">Page {page}</button>
-      {page < totalPage && <button className="btn join-item">...</button>}
       <button className="btn join-item" onClick={handleNextPage} disabled={page >= totalPage}>
         »
       </button>
